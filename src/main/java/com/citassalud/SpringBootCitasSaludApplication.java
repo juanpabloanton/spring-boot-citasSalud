22eb5db92d20e3290ed66a5a2d4395a97567f50d
Juan Anton <juanpabloantonyagual@gmail.com>
2026-07-02 23:35:25 -0500

    subida de repositorio

---
+++ src/main/java/com/citassalud/SpringBootCitasSaludApplication.java
@@ -0,0 +1,13 @@
+package com.citassalud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCitasSaludApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCitasSaludApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citassalud/SpringBootCitasSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citassalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCitasSaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
